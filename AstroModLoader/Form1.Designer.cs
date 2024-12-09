b3424ca42ecaa96ff11930acf2ab4fe673b38c04
atenfyr <hello@atenfyr.com>
2024-12-09 00:07:44 -0600

    fix integrating label sizing x2

--- AstroModLoader/Form1.Designer.cs
+++ AstroModLoader/Form1.Designer.cs
@@ -55,11 +55,11 @@
             // 
             headerLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
             headerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            headerLabel.Location = new System.Drawing.Point(26, 0);
+            headerLabel.Location = new System.Drawing.Point(39, 0);
             headerLabel.Margin = new System.Windows.Forms.Padding(0);
             headerLabel.Name = "headerLabel";
-            headerLabel.Padding = new System.Windows.Forms.Padding(10, 10, 3, 3);
-            headerLabel.Size = new System.Drawing.Size(416, 30);
+            headerLabel.Padding = new System.Windows.Forms.Padding(15, 15, 4, 4);
+            headerLabel.Size = new System.Drawing.Size(624, 45);
             headerLabel.TabIndex = 0;
             headerLabel.Text = "Mods:";
             headerLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -70,10 +70,11 @@
             modInfo.AutoSize = true;
             modInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
             modInfo.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
-            modInfo.Location = new System.Drawing.Point(12, 320);
-            modInfo.MaximumSize = new System.Drawing.Size(400, 0);
+            modInfo.Location = new System.Drawing.Point(18, 480);
+            modInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            modInfo.MaximumSize = new System.Drawing.Size(600, 0);
             modInfo.Name = "modInfo";
-            modInfo.Size = new System.Drawing.Size(83, 17);
+            modInfo.Size = new System.Drawing.Size(125, 25);
             modInfo.TabIndex = 3;
             modInfo.Text = "Testing 123";
             modInfo.LinkClicked += modInfo_LinkClicked;
@@ -87,9 +88,10 @@
             footerPanel.Controls.Add(exitButton);
             footerPanel.Controls.Add(settingsButton);
             footerPanel.Controls.Add(playButton);
-            footerPanel.Location = new System.Drawing.Point(0, 441);
+            footerPanel.Location = new System.Drawing.Point(0, 662);
+            footerPanel.Margin = new System.Windows.Forms.Padding(4);
             footerPanel.Name = "footerPanel";
-            footerPanel.Size = new System.Drawing.Size(468, 50);
+            footerPanel.Size = new System.Drawing.Size(702, 75);
             footerPanel.TabIndex = 5;
             // 
             // integratingLabel
@@ -97,10 +99,12 @@
             integratingLabel.ActiveLinkColor = System.Drawing.Color.Red;
             integratingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
             integratingLabel.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
-            integratingLabel.Location = new System.Drawing.Point(230, 12);
-            integratingLabel.MaximumSize = new System.Drawing.Size(400, 0);
+            integratingLabel.LinkColor = System.Drawing.Color.FromArgb(225, 225, 225);
+            integratingLabel.Location = new System.Drawing.Point(345, 18);
+            integratingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            integratingLabel.MaximumSize = new System.Drawing.Size(600, 0);
             integratingLabel.Name = "integratingLabel";
-            integratingLabel.Size = new System.Drawing.Size(145, 26);
+            integratingLabel.Size = new System.Drawing.Size(218, 39);
             integratingLabel.TabIndex = 6;
             integratingLabel.Text = "Integrating...";
             integratingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -112,10 +116,11 @@
             exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             exitButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
             exitButton.ForeColor = System.Drawing.Color.FromArgb(225, 225, 225);
-            exitButton.Location = new System.Drawing.Point(381, 12);
-            exitButton.MinimumSize = new System.Drawing.Size(0, 26);
+            exitButton.Location = new System.Drawing.Point(572, 18);
+            exitButton.Margin = new System.Windows.Forms.Padding(4);
+            exitButton.MinimumSize = new System.Drawing.Size(0, 39);
             exitButton.Name = "exitButton";
-            exitButton.Size = new System.Drawing.Size(75, 26);
+            exitButton.Size = new System.Drawing.Size(112, 39);
             exitButton.TabIndex = 6;
             exitButton.Text = "Exit";
             exitButton.UseVisualStyleBackColor = false;
@@ -128,10 +133,11 @@
             settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             settingsButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
             settingsButton.ForeColor = System.Drawing.Color.FromArgb(225, 225, 225);
-            settingsButton.Location = new System.Drawing.Point(92, 12);
-            settingsButton.MinimumSize = new System.Drawing.Size(0, 26);
+            settingsButton.Location = new System.Drawing.Point(138, 18);
+            settingsButton.Margin = new System.Windows.Forms.Padding(4);
+            settingsButton.MinimumSize = new System.Drawing.Size(0, 39);
             settingsButton.Name = "settingsButton";
-            settingsButton.Size = new System.Drawing.Size(82, 26);
+            settingsButton.Size = new System.Drawing.Size(123, 39);
             settingsButton.TabIndex = 5;
             settingsButton.Text = "Settings...";
             settingsButton.UseVisualStyleBackColor = false;
@@ -144,10 +150,11 @@
             playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             playButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
             playButton.ForeColor = System.Drawing.Color.FromArgb(225, 225, 225);
-            playButton.Location = new System.Drawing.Point(12, 12);
-            playButton.MinimumSize = new System.Drawing.Size(0, 26);
+            playButton.Location = new System.Drawing.Point(18, 18);
+            playButton.Margin = new System.Windows.Forms.Padding(4);
+            playButton.MinimumSize = new System.Drawing.Size(0, 39);
             playButton.Name = "playButton";
-            playButton.Size = new System.Drawing.Size(75, 26);
+            playButton.Size = new System.Drawing.Size(112, 39);
             playButton.TabIndex = 4;
             playButton.Text = "Play";
             playButton.UseVisualStyleBackColor = false;
@@ -161,10 +168,11 @@
             modPanel.Controls.Add(refresh);
             modPanel.Controls.Add(loadButton);
             modPanel.Controls.Add(syncButton);
-            modPanel.Location = new System.Drawing.Point(0, 35);
+            modPanel.Location = new System.Drawing.Point(0, 52);
+            modPanel.Margin = new System.Windows.Forms.Padding(4);
             modPanel.Name = "modPanel";
-            modPanel.Padding = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            modPanel.Size = new System.Drawing.Size(460, 270);
+            modPanel.Padding = new System.Windows.Forms.Padding(2);
+            modPanel.Size = new System.Drawing.Size(690, 405);
             modPanel.TabIndex = 1;
             // 
             // dataGridView1
@@ -191,13 +199,14 @@
             dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
             dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             dataGridView1.EnableHeadersVisualStyles = false;
-            dataGridView1.Location = new System.Drawing.Point(12, 0);
+            dataGridView1.Location = new System.Drawing.Point(18, 0);
+            dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             dataGridView1.MultiSelect = false;
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersVisible = false;
             dataGridView1.RowHeadersWidth = 62;
             dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            dataGridView1.Size = new System.Drawing.Size(435, 225);
+            dataGridView1.Size = new System.Drawing.Size(652, 338);
             dataGridView1.TabIndex = 0;
             dataGridView1.CellPainting += dataGridView1_CellPainting;
             dataGridView1.KeyDown += dataGridView1_KeyDown;
@@ -209,10 +218,11 @@
             refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             refresh.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
             refresh.ForeColor = System.Drawing.Color.FromArgb(225, 225, 225);
-            refresh.Location = new System.Drawing.Point(12, 236);
-            refresh.MinimumSize = new System.Drawing.Size(0, 26);
+            refresh.Location = new System.Drawing.Point(18, 354);
+            refresh.Margin = new System.Windows.Forms.Padding(4);
+            refresh.MinimumSize = new System.Drawing.Size(0, 39);
             refresh.Name = "refresh";
-            refresh.Size = new System.Drawing.Size(75, 26);
+            refresh.Size = new System.Drawing.Size(112, 39);
             refresh.TabIndex = 1;
             refresh.Text = "Refresh";
             refresh.UseVisualStyleBackColor = false;
@@ -225,10 +235,11 @@
             loadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             loadButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
             loadButton.ForeColor = System.Drawing.Color.FromArgb(225, 225, 225);
-            loadButton.Location = new System.Drawing.Point(92, 236);
-            loadButton.MinimumSize = new System.Drawing.Size(0, 26);
+            loadButton.Location = new System.Drawing.Point(138, 354);
+            loadButton.Margin = new System.Windows.Forms.Padding(4);
+            loadButton.MinimumSize = new System.Drawing.Size(0, 39);
             loadButton.Name = "loadButton";
-            loadButton.Size = new System.Drawing.Size(75, 26);
+            loadButton.Size = new System.Drawing.Size(112, 39);
             loadButton.TabIndex = 2;
             loadButton.Text = "Profiles...";
             loadButton.UseVisualStyleBackColor = false;
@@ -241,10 +252,11 @@
             syncButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             syncButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
             syncButton.ForeColor = System.Drawing.Color.FromArgb(225, 225, 225);
-            syncButton.Location = new System.Drawing.Point(338, 236);
-            syncButton.MinimumSize = new System.Drawing.Size(0, 26);
+            syncButton.Location = new System.Drawing.Point(507, 354);
+            syncButton.Margin = new System.Windows.Forms.Padding(4);
+            syncButton.MinimumSize = new System.Drawing.Size(0, 39);
             syncButton.Name = "syncButton";
-            syncButton.Size = new System.Drawing.Size(109, 26);
+            syncButton.Size = new System.Drawing.Size(164, 39);
             syncButton.TabIndex = 3;
             syncButton.Text = "Sync from IP";
             syncButton.UseVisualStyleBackColor = false;
@@ -273,18 +285,19 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             BackColor = System.Drawing.Color.FromArgb(40, 42, 45);
-            ClientSize = new System.Drawing.Size(469, 491);
+            ClientSize = new System.Drawing.Size(704, 736);
             Controls.Add(modPanel);
             Controls.Add(footerPanel);
             Controls.Add(modInfo);
             Controls.Add(headerLabel);
             ForeColor = System.Drawing.Color.FromArgb(225, 225, 225);
+            Margin = new System.Windows.Forms.Padding(4);
             MaximizeBox = false;
-            MaximumSize = new System.Drawing.Size(898, 694);
-            MinimumSize = new System.Drawing.Size(483, 509);
+            MaximumSize = new System.Drawing.Size(1336, 1013);
+            MinimumSize = new System.Drawing.Size(714, 736);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
