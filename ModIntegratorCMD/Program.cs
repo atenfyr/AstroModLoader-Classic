3f71ca3d990e3dbd95dd676199a0d180e57b8c99
atenfyr <atenefyr@gmail.com>
2024-12-10 19:30:21 -0600

    add benchmarking for integrator cmd

--- ModIntegratorCMD/Program.cs
+++ ModIntegratorCMD/Program.cs
@@ -22,62 +22,86 @@ namespace ModIntegratorCMD
             }
 
             Console.ReadKey();*/
+#if DEBUG
+            args = ["benchmark", Path.Combine(Environment.GetEnvironmentVariable("LocalAppData") ?? throw new InvalidOperationException(), @"Astro\Saved\Paks"), @"D:\Games\steamapps\common\ASTRONEER\Astro\Content\Paks\"];
+#endif
 
-            /*{
-                Stopwatch extractingTimer = new Stopwatch();
-                extractingTimer.Start();
+                /*{
+                    Stopwatch extractingTimer = new Stopwatch();
+                    extractingTimer.Start();
 
-                string pakPath = @"C:\Program Files (x86)\Steam\steamapps\common\ASTRONEER\Astro\Content\Paks\Astro-WindowsNoEditor.pak";
+                    string pakPath = @"C:\Program Files (x86)\Steam\steamapps\common\ASTRONEER\Astro\Content\Paks\Astro-WindowsNoEditor.pak";
 
-                string extractingDir = Path.Combine(Path.GetDirectoryName(pakPath), Path.GetFileNameWithoutExtension(pakPath));
-                Directory.CreateDirectory(extractingDir);
-                using (FileStream f = new FileStream(pakPath, FileMode.Open, FileAccess.Read))
-                {
-                    PakExtractor mainExtractor = new PakExtractor(new BinaryReader(f));
-                    IReadOnlyList<string> allPaths = mainExtractor.GetAllPaths(); // Get a list of every path that is contained within this pak file. The provided mount point is ignored
-                    foreach (string path in allPaths)
+                    string extractingDir = Path.Combine(Path.GetDirectoryName(pakPath), Path.GetFileNameWithoutExtension(pakPath));
+                    Directory.CreateDirectory(extractingDir);
+                    using (FileStream f = new FileStream(pakPath, FileMode.Open, FileAccess.Read))
                     {
-                        Console.WriteLine("Extracting " + path);
+                        PakExtractor mainExtractor = new PakExtractor(new BinaryReader(f));
+                        IReadOnlyList<string> allPaths = mainExtractor.GetAllPaths(); // Get a list of every path that is contained within this pak file. The provided mount point is ignored
+                        foreach (string path in allPaths)
+                        {
+                            Console.WriteLine("Extracting " + path);
 
-                        string writingPathName = Path.Combine(extractingDir, path);
-                        Directory.CreateDirectory(Path.GetDirectoryName(writingPathName));
+                            string writingPathName = Path.Combine(extractingDir, path);
+                            Directory.CreateDirectory(Path.GetDirectoryName(writingPathName));
 
-                        byte[] allPathData = mainExtractor.ReadRaw(path, true); // Read the bytes of this particular asset based off its path within the pak file
-                        File.WriteAllBytes(writingPathName, allPathData);
+                            byte[] allPathData = mainExtractor.ReadRaw(path, true); // Read the bytes of this particular asset based off its path within the pak file
+                            File.WriteAllBytes(writingPathName, allPathData);
+                        }
                     }
-                }
 
-                extractingTimer.Stop();
-                Console.WriteLine("Finished extracting! Took " + ((double)extractingTimer.Elapsed.Ticks / TimeSpan.TicksPerSecond) + " seconds in total.");
-                return;
-            }*/
+                    extractingTimer.Stop();
+                    Console.WriteLine("Finished extracting! Took " + ((double)extractingTimer.Elapsed.Ticks / TimeSpan.TicksPerSecond) + " seconds in total.");
+                    return;
+                }*/
 
-            /*{
-                Stopwatch testTimer = new Stopwatch();
-                string pakPath = @"C:\Program Files (x86)\Steam\steamapps\common\ASTRONEER\Astro\Content\Paks\Astro-WindowsNoEditor.pak";
+                /*{
+                    Stopwatch testTimer = new Stopwatch();
+                    string pakPath = @"C:\Program Files (x86)\Steam\steamapps\common\ASTRONEER\Astro\Content\Paks\Astro-WindowsNoEditor.pak";
 
-                using (FileStream f = new FileStream(pakPath, FileMode.Open, FileAccess.Read))
-                {
-                    testTimer.Start();
-                    PakExtractor mainExtractor = new PakExtractor(new BinaryReader(f));
-                    testTimer.Stop();
-                    Console.WriteLine("Parsed index in " + ((double)testTimer.Elapsed.Ticks / TimeSpan.TicksPerMillisecond) + " ms");
-
-                    IReadOnlyList<string> allPaths = mainExtractor.GetAllPaths();
-                    int numTests = 5000;
-                    testTimer.Reset();
-                    testTimer.Start();
-                    for (int i = 0; i < numTests; i++)
+                    using (FileStream f = new FileStream(pakPath, FileMode.Open, FileAccess.Read))
                     {
-                        byte[] allPathData = mainExtractor.ReadRaw(allPaths[i]);
-                    }
-                    testTimer.Stop();
-                    Console.WriteLine("~" + ((double)testTimer.Elapsed.Ticks / TimeSpan.TicksPerMillisecond) / numTests + " ms per asset read");
-                    //Console.ReadLine();
-                }
+                        testTimer.Start();
+                        PakExtractor mainExtractor = new PakExtractor(new BinaryReader(f));
+                        testTimer.Stop();
+                        Console.WriteLine("Parsed index in " + ((double)testTimer.Elapsed.Ticks / TimeSpan.TicksPerMillisecond) + " ms");
 
+                        IReadOnlyList<string> allPaths = mainExtractor.GetAllPaths();
+                        int numTests = 5000;
+                        testTimer.Reset();
+                        testTimer.Start();
+                        for (int i = 0; i < numTests; i++)
+                        {
+                            byte[] allPathData = mainExtractor.ReadRaw(allPaths[i]);
+                        }
+                        testTimer.Stop();
+                        Console.WriteLine("~" + ((double)testTimer.Elapsed.Ticks / TimeSpan.TicksPerMillisecond) / numTests + " ms per asset read");
+                        //Console.ReadLine();
+                    }
+
+                    return;
+                }*/
+
+
+            if (args.Length == 3 && args[0] == "benchmark")
+            {
+                ModIntegrator us = new ModIntegrator()
+                {
+                    RefuseMismatchedConnections = true,
+                    //OptionalModIDs = new List<string> { "AstroChat" }
+                };
+
+                int TRIALS = 10;
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                for (int i = 0; i < TRIALS; i++)
+                {
+                    us.IntegrateMods(args[1], args[2]);
+                }
+                stopWatch.Stop();
+                Console.WriteLine("Finished integrating! Took " + ((double)stopWatch.Elapsed.Ticks / TimeSpan.TicksPerMillisecond / TRIALS) + " ms per trial for " + TRIALS + " trials.");
                 return;
-            }*/
+            }
 
             if (args.Length != 2)
             {
@@ -94,18 +118,20 @@ namespace ModIntegratorCMD
                 return;
             }
 
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-
-            ModIntegrator us = new ModIntegrator()
             {
-                RefuseMismatchedConnections = true,
-                //OptionalModIDs = new List<string> { "AstroChat" }
-            };
-            us.IntegrateMods(args[0], args[1]);
-            stopWatch.Stop();
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
 
-            Console.WriteLine("Finished integrating! Took " + ((double)stopWatch.Elapsed.Ticks / TimeSpan.TicksPerMillisecond) + " ms in total.");
+                ModIntegrator us = new ModIntegrator()
+                {
+                    RefuseMismatchedConnections = true,
+                    //OptionalModIDs = new List<string> { "AstroChat" }
+                };
+                us.IntegrateMods(args[0], args[1]);
+                stopWatch.Stop();
+
+                Console.WriteLine("Finished integrating! Took " + ((double)stopWatch.Elapsed.Ticks / TimeSpan.TicksPerMillisecond) + " ms in total.");
+            }
         }
     }
 }
